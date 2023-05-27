4205c10b70412f804b852e858d6f4245afb8187b
sandeepthadoju0451 <48976530+sandeepthadoju0451@users.noreply.github.com>
2023-05-27 12:31:36 +0530

    Add files via upload

---
+++ Object Repository/RadarBox/Search/lblNoResult.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lblNoResult</name>
+   <tag></tag>
+   <elementGuidId>be63b9c9-ce27-4d42-99a4-e8f7f4b42fde</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='results']//div[contains(text(),'No results')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
